e3e6f22ab01bd1fd10a02c5c626e672a51fbce4e
Mohamed Talal <medoalashkam5@gmail.com>
2026-07-30 21:18:53 +0300

    Temporarily expose errors

--- backend/index.js
+++ backend/index.js
@@ -65,7 +65,8 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
     error: 'Internal Server Error',
-    message: process.env.NODE_ENV === 'development' ? err.message : 'Something went wrong!'
+    message: err.message,
+    stack: err.stack
   });
 });
 
