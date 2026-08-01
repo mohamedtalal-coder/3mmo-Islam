2557fae93d7c2b87fdb6ec30a0177261b0634cca
Mohamed Talal <medoalashkam5@gmail.com>
2026-08-01 19:42:04 +0300

    fix: increase express body parser limit to 50mb to allow image uploads

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,8 @@ app.use(cors({
   credentials: true // Allow cookies to be sent
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
